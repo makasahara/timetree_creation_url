54e4c82a94b89b82765ecefe92bea6debf4668dc
masashi.kasahara <ma.kasahara2369@gmail.com>
2024-10-15 23:27:56 +0900

    メモ、場所、添付URLを追加。

--- src/components/organisms/form/index.tsx
+++ src/components/organisms/form/index.tsx
@@ -3,19 +3,37 @@ import Heading from "../../atoms/heading";
 import Input from "../../atoms/input";
 import FormPart from "../../molecules/formPart";
 import CheckBtn from "../../atoms/checkBtn";
+import TextArea from "../../atoms/textArea";
 import { fieldset } from "./style";
+import { placeholders } from "./define";
 
 const Form = () => {
+
   return (
     <form>
       <Heading text="予定作成URLの生成" />
       <fieldset css={fieldset}>
         <FormPart id="title" labelText="予定のタイトル" isRequired={true}>
-          <Input placeholder="TimeTree Day" />
+          <Input placeholder={placeholders.title} />
         </FormPart>
       </fieldset>
       <fieldset css={fieldset}>
         <CheckBtn text="終日" />
+      </fieldset>
+      <fieldset css={fieldset}>
+        <FormPart id="memo" labelText="メモ">
+          <TextArea placeholder={placeholders.memo} />
+        </FormPart>
+      </fieldset>
+      <fieldset css={fieldset}>
+        <FormPart id="place" labelText="場所">
+          <Input placeholder={placeholders.place} />
+        </FormPart>
+      </fieldset>
+      <fieldset css={fieldset}>
+        <FormPart id="attachedUrl" labelText="添付URL">
+          <Input placeholder={placeholders.attachedUrl} />
+        </FormPart>
       </fieldset>
     </form>
   );
